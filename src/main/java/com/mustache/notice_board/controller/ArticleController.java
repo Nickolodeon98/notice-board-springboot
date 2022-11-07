e868209de378dd968414a176b1ddeaf9c344ca51
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-07 14:14:52 +0900

    Feat: ArticleController 추가 및 새 페이지에 보여질 뷰 new.mustache 에 구현

---
+++ src/main/java/com/mustache/notice_board/controller/ArticleController.java
@@ -0,0 +1,16 @@
+package com.mustache.notice_board.controller;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class ArticleController {
+
+    @GetMapping("/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+}
