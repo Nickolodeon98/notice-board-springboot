6b04be8d8f24823f4ba38e87ec7fff74c6692769
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-07 11:48:03 +0900

    Feat: name 과 id 를 url 에서 받아서 클라이언트에게 출력할 수 있도록 컨트롤러 및 뷰 변경

--- src/main/java/com/mustache/notice_board/controller/MustacheController.java
+++ src/main/java/com/mustache/notice_board/controller/MustacheController.java
@@ -4,14 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /* RestController 대신 Controller 어노테이션이 들어간다. */
 @Controller
 public class MustacheController {
 
-    @GetMapping(value="/salute/{id}")
-    public String mustacheCon(Model model, @PathVariable String id) {
-        model.addAttribute("username", id); // view 에 값을 넘기겠다는 의미
+    @GetMapping(value="/salute")
+    public String mustacheCon(Model model, @RequestParam String name, @RequestParam String id) {
+        model.addAttribute("username", name); // view 에 값을 넘기겠다는 의미
+        model.addAttribute("id", id);
         return "greetings"; // greetings 라는 이름의 view 를 리턴한다
     }
 
