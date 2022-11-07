0339246f299f39075a2da8d69e9d41556252cca5
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-07 14:28:42 +0900

    Feat: ArticleDto 구현하여 폼에서 제목과 내용을 입력 받아 변수에 저장하는 DTO 생성 및 ArticleController 에서 POST 방식을 사용해 ArticleDto 객체를 생성하고 로그로 출력하도록 기능 추가 완료

--- src/main/java/com/mustache/notice_board/controller/ArticleController.java
+++ src/main/java/com/mustache/notice_board/controller/ArticleController.java
@@ -1,16 +1,24 @@
 package com.mustache.notice_board.controller;
 
+import com.mustache.notice_board.domain.dto.ArticleDto;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Slf4j
+@RequestMapping("/articles")
 public class ArticleController {
 
-    @GetMapping("/articles/new")
+    @GetMapping("/new")
     public String newArticleForm() {
         return "articles/new";
+    }
+
+    @PostMapping("/posts")
+    public String createArticle(ArticleDto articleDto) {
+        log.info(articleDto.toString());
+        return articleDto.toString();
     }
 }
